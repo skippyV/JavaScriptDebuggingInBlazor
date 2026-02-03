fffd25e00d1958832be2a4b941e5d495a90bb649
skippyV <skippyV@users.noreply.github.com>
2026-02-03 12:43:25 -0500

    inital commit

---
+++ BlazorJavaScriptTest/Components/Pages/JavaScriptTest.razor.js
@@ -0,0 +1,12 @@
+﻿export function showPrompt2(message) {
+    return prompt(message, 'Type your name here');
+}
+
+export function alertUser() {
+    alert('The button was selected!');
+}
+
+export function addHandlers() {
+    const btn = document.getElementById("btnId");
+    btn.addEventListener("click", alertUser);
+}
---
+++ README - ISSUE.txt
@@ -0,0 +1,4 @@
+
+I am unable to hit a breakpoint in JavaScript loaded from a Blazor Web App. (.Net 10)
+Simple functions work but why can I not break into them?
+I have ScriptDebugging Enabled in Visual Studio 
